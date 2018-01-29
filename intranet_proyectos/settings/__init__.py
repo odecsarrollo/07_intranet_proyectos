e3be1505aee3f684e41e8463414f74d65c87e5f3
Fabio A MacPro <fabio.garcia.sanchez@gmail.com>
2018-01-29 10:21:57 -0500

    Initial settings

--- intranet_proyectos/settings/__init__.py
+++ intranet_proyectos/settings/__init__.py
@@ -1,8 +1,6 @@
 from .base import *
-from .local import *
-
-# try:
-#     with open("secretsLocal.json") as f:
-#         from .local import *
-# except:
-#     from .production import *
+try:
+    with open("secretsLocal.json") as f:
+        from .local import *
+except:
+    from .production import *
