0cbaa3bb41e966f43d78c5e06647d8e4ddfb4511
Fabio A MacPro <fabio.garcia.sanchez@gmail.com>
2018-01-29 13:30:08 -0500

    Items Biable

--- cguno/admin.py
+++ cguno/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 
 from .models import ItemsBiable
 
+
+class ItemBiableAdmin(admin.ModelAdmin):
+    list_display = [
+        'id_item',
+        'id_referencia',
+        'descripcion',
+        'activo',
+        'nombre_tercero',
+        'desc_item_padre',
+        'unidad_medida_inventario',
+        'id_procedencia',
+        'ultimo_costo'
+    ]
+
+    list_filter = ['activo']
+
+
 admin.site.register(ItemsBiable)
