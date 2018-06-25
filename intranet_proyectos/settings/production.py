9513424ec35f285fd9941196fd014ec03f9f06b2
Fabio A MacPro <fabio.garcia.sanchez@gmail.com>
2018-06-25 14:00:58 -0500

    Deploy en aws

--- intranet_proyectos/settings/production.py
+++ intranet_proyectos/settings/production.py
@@ -125,3 +125,16 @@ EMAIL_USE_SSL = str_to_bool(os.environ["EMAIL_USE_SSL"])
 
 DEFAULT_FROM_EMAIL = os.environ["DEFAULT_FROM_EMAIL"]
 ########## END EMAIL CONFIGURATION
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'assets/bundles/dist/',
+        'STATS_FILE': os.path.join(SITE_ROOT, 'webpack-stats-prod.json'),
+    }
+}
