dfd66d08bca7e2be4b1c87581b23109718b4c0fb
Fabio A MacPro <fabio.garcia.sanchez@gmail.com>
2018-09-10 11:04:55 -0500

    Filtros de colores y mejoras para la tubería de ventas

---
+++ static/assets/js/01_actions/especificas/proyectos/fasesAction.jsx
@@ -0,0 +1,8 @@
+export const FASE_TYPES = {
+    create: 'create_fase',
+    delete: 'delete_fase',
+    fetch_all: 'fetch_fases',
+    fetch: 'fetch_fase',
+    clear: 'clear_fases',
+    update: 'update_fase',
+};
