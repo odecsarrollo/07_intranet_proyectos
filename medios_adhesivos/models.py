8a7180f3f7fb78c39b124a6eebe6cf9a97a01884
Fabio A MacPro <fabio.garcia.sanchez@gmail.com>
2019-05-28 12:37:11 -0500

    Mejora CRUD y subida de archivos por formData

--- medios_adhesivos/models.py
+++ medios_adhesivos/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class Adhesivo(models.Model):
-
     def archivo_upload_to(instance, filename):
         return "imagenes/medios/etiquetas/%s" % (filename)
 
-    codigo = models.CharField(max_length=100, unique = True)
+    codigo = models.CharField(max_length=100, unique=True)
     alto = models.DecimalField(max_digits=12, decimal_places=2, default=0)
     ancho = models.DecimalField(max_digits=12, decimal_places=2, default=0)
-    color = models.CharField(null= True, max_length=10)
-    stock_min = models.IntegerField(null= True, default=0)
-    descripcion = models.CharField(null= True, max_length=500)
-    imagen = models.FileField(null= True, upload_to=archivo_upload_to)
+    color = models.CharField(null=True, max_length=10)
+    stock_min = models.IntegerField(null=True, default=0)
+    descripcion = models.CharField(null=True, max_length=500)
+    imagen = models.FileField(null=True, upload_to=archivo_upload_to)
     sticker = models.BooleanField()
