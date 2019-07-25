2ac69bdb70b736f8c524399a0a99858aa36a7118
Fabio A MacPro <fabio.garcia.sanchez@gmail.com>
2019-07-25 12:41:47 -0500

    Mejora a la configuración

--- contabilidad_anticipos/services.py
+++ contabilidad_anticipos/services.py
@@ -100,7 +100,7 @@ def proforma_anticipo_enviar(
     print(proforma_anticipo.documento.archivo.__dict__)
     # msg.attach_file(proforma_anticipo.documento.archivo)
     archivos_para_enviar = proforma_anticipo.documentos.filter(enviar_por_correo=True)
-    # [msg.attach_file(archivo.archivo.path) for archivo in archivos_para_enviar]
+    [msg.attach_file(archivo.archivo.path) for archivo in archivos_para_enviar]
 
     try:
         pass
