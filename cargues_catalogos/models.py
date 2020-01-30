289eef6e6f03fd2018e8a1f640738b493e4fa722
Fabio A MacPro <fabio.garcia.sanchez@gmail.com>
2020-01-30 12:32:03 -0500

    - Se crea aplicacion de colaboradores y se relaciona con cargues catalogos

--- cargues_catalogos/models.py
+++ cargues_catalogos/models.py
@@ -98,7 +98,7 @@ class ColaboradorCatalogo(models.Model):
         null=True
     )
     sistema_informacion = models.ForeignKey(SistemaInformacionOrigen, on_delete=models.PROTECT)
-    cedula = models.CharField(max_length=20, unique=True)
+    cedula = models.CharField(max_length=20)
     nombres = models.CharField(max_length=200, null=True)
     apellidos = models.CharField(max_length=200, null=True)
     tercero_id = models.BigIntegerField(null=True)
