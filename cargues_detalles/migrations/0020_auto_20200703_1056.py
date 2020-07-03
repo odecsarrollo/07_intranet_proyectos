8afe0a1dc65e9e7fd2d48987e49c079673debaf7
Fabio A MacPro <fabio.garcia.sanchez@gmail.com>
2020-07-03 11:21:38 -0500

    - Permite redefinir los tipos de documentos de las facturas

--- cargues_detalles/migrations/0020_auto_20200703_1056.py
+++ cargues_detalles/migrations/0020_auto_20200703_1056.py
@@ -4,15 +4,13 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('sistema_informacion_origen', '0004_auto_20190610_1453'),
-        ('cargues_detalles', '0019_auto_20200703_1053'),
+        ('cargues_detalles', '0020_auto_20200703_1056'),
     ]
 
     operations = [
         migrations.AlterUniqueTogether(
             name='facturadetalle',
-            unique_together={('sistema_informacion', 'tipo_documento_original', 'nro_documento')},
+            unique_together=set(),
         ),
     ]
