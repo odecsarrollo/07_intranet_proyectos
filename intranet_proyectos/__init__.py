7de6895136e356d262324b48b837d2f80a091ab8
Fabio A MacPro <fabio.garcia.sanchez@gmail.com>
2020-07-23 08:43:33 -0500

    - Envio de SMS en Ventas Proyectos - Arreglo de Avisos al crear clientes cuando el nit esta duplicado

--- intranet_proyectos/__init__.py
+++ intranet_proyectos/__init__.py
@@ -1,7 +1,7 @@
-from __future__ import absolute_import, unicode_literals
-
-# Esto sirve para que la aplicación de Celery se creé siempre
-# que Django se inicie.
-from .celeryapp import app as celery_app
-
-__all__ = ('celery_app',)
+# from __future__ import absolute_import, unicode_literals
+#
+# # Esto sirve para que la aplicación de Celery se creé siempre
+# # que Django se inicie.
+# from .celeryapp import app as celery_app
+#
+# __all__ = ('celery_app',)
